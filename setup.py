f20624a1b18af70c2028bbef2c7f971b253b476d
Numa Dhamani <numadhamani@gmail.com>
2019-01-28 10:36:59 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='d3m_croc',
                         'pandas == 0.23.4',
                         'tesserocr == 2.3.1',
                         'spacy == 2.0.11',
-                        'requests == 2.20.0',
+                        'requests >= 2.18.4, <= 2.20.0',
                         'numpy >= 1.13.3',
                         'Pillow >= 5.1.0'],
       cmdclass={
